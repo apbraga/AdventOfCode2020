1534ce9fe9296cda4eca6c8cbff047f05a2daa7b
Alex Braga <alexbraga101@gmail.com>
2020-12-26 23:29:42 -0300

    Day One Part Two

--- Day1.py
+++ Day1.py
@@ -1,13 +1,28 @@
 import numpy as np
-
+# bring in input data
 with open("day1_input.txt", "r", encoding="utf-8") as g:
     data = list(map(int, g.readlines()))
-
 data = np.array(data)
-remainder = 2020 - data
+# set number of numbers to sum
+param = 3
 
-for x in data:
-    if x in remainder:
-        multiply = x * (2020-x)
-        print(multiply)
-        break
+data = np.sort(data)
+data[0]
+# a + b + c = 2020
+# a * b * c = ???
+
+# PART 2
+for i in range(len(data)):
+    for j in range(len(data)):
+        if(2020 - data[i] - data[j] in data):
+            multiply = data[i] * data[j] * (2020 - data[i] - data[j])
+            print(multiply)
+            break
+
+#PART 1
+#remainder = 2020 - data
+#for x in data:
+#    if x in remainder:
+#        multiply = x * (2020-x)
+#        print(multiply)
+#        break
